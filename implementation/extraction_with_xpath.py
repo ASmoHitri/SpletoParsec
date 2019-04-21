7aff4f10c1a6adf7109ce9c97c9a0b3b699a4cdc
Maja Umek <mu2729@student.uni-lj.si>
2019-04-21 13:47:32 +0200

    Minor code changes

--- implementation/extraction_with_xpath.py
+++ implementation/extraction_with_xpath.py
@@ -11,11 +11,11 @@ def extract_from_overstock(file_name: str):
     html_tree = html.fromstring(html_content)
     items = html_tree.xpath('//td[@valign="top"][a]')
     for item in items:
-        data_record = {
-            "Title": str(item.xpath('./a/b/text()')[0]),
-            "ListPrice": str(item.xpath('.//tbody/tr/td[2]/s/text()')[0]),
-            "Price": str(item.xpath('.//tbody/tr/td/span[@class="bigred"]/b/text()')[0])
-        }
+        data_record = {}
+        data_record["Title"] = str(item.xpath('./a/b/text()')[0])
+        data_record["ListPrice"] = str(item.xpath('.//tbody/tr/td[2]/s/text()')[0])
+        data_record["Price"] = str(item.xpath('.//tbody/tr/td/span[@class="bigred"]/b/text()')[0])
+
         saving_all = str(item.xpath('.//tbody/tr/td[2]/span/text()')[0])
         data_record['Saving'] = re.search("\$[0-9.,]*", saving_all).group(0)
         data_record['SavingPercent'] = re.search("\((.*)\)", saving_all).group(1)
@@ -23,7 +23,6 @@ def extract_from_overstock(file_name: str):
         content_string = str(item.xpath('.//span[@class="normal"]/text()')[0])
         data_record["Content"] = re.sub("\.\s*$", ".", content_string)      # remove trailing whitespace
 
-        # print(html.tostring(item))
         data_records.append(data_record)
     output = json.dumps(data_records, indent=2)
     print(output)
