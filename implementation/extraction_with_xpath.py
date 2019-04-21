af4d5694f23cd84845b30e941634b2f9339ba9b4
Maja Umek <mu2729@student.uni-lj.si>
2019-04-21 13:41:29 +0200

    Overstock xPath extraction

---
+++ implementation/extraction_with_xpath.py
@@ -0,0 +1,34 @@
+import re
+import json
+from lxml import html
+
+base_content_path = "../input/"
+
+
+def extract_from_overstock(file_name: str):
+    data_records = []
+    html_content = open(base_content_path + file_name, 'r').read()
+    html_tree = html.fromstring(html_content)
+    items = html_tree.xpath('//td[@valign="top"][a]')
+    for item in items:
+        data_record = {
+            "Title": str(item.xpath('./a/b/text()')[0]),
+            "ListPrice": str(item.xpath('.//tbody/tr/td[2]/s/text()')[0]),
+            "Price": str(item.xpath('.//tbody/tr/td/span[@class="bigred"]/b/text()')[0])
+        }
+        saving_all = str(item.xpath('.//tbody/tr/td[2]/span/text()')[0])
+        data_record['Saving'] = re.search("\$[0-9.,]*", saving_all).group(0)
+        data_record['SavingPercent'] = re.search("\((.*)\)", saving_all).group(1)
+
+        content_string = str(item.xpath('.//span[@class="normal"]/text()')[0])
+        data_record["Content"] = re.sub("\.\s*$", ".", content_string)      # remove trailing whitespace
+
+        # print(html.tostring(item))
+        data_records.append(data_record)
+    output = json.dumps(data_records, indent=2)
+    print(output)
+    return output
+
+
+if __name__ == "__main__":
+    extract_from_overstock("overstock.com/jewelry01.html")
